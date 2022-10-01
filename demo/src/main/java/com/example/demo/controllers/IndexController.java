4cec0bfb872beed59a859eac253923223df35328
Ylenia Díaz Trinidad <ylenia.d.23@gmail.com>
2022-10-01 20:09:01 +0000

    First Hello World

---
+++ demo/src/main/java/com/example/demo/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        log.info("Index Controller");
+        return "Hello World!";
+    }
+}
